70b6f0db0087184db3f2cb2da4d9064a88791ee5
vegimik <vegimik@gmail.com>
2022-11-23 18:37:58 +0100

    Asynchronous Lazy Initilaization

---
+++ ParallelProgramming.NET/7. AsynchronousProgramming/6_AsynchronousLazyInit.cs
@@ -0,0 +1,50 @@
+﻿using BenchmarkDotNet.Attributes;
+using Nito.AsyncEx;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelProgramming.NET._7._AsynchronousProgramming
+{
+    internal class _6_AsynchronousLazyInit
+    {
+        public class Staff
+        {
+            private static int value;
+            private readonly Lazy<Task<int>> AutoIncValue = new Lazy<Task<int>>(
+                 async () =>
+                {
+                    await Task.Delay(1000).ConfigureAwait(false);
+                    return value++;
+                });
+
+            private readonly Lazy<Task<int>> AutoIncValue2 = new Lazy<Task<int>>(() => Task.Run(async () =>
+            {
+                await Task.Delay(1000);
+                return value++;
+            }));
+
+            private AsyncLazy<Task<int>> AutoIncValue3 = new AsyncLazy<Task<int>>(
+                async () =>
+                {
+                    await Task.Delay(1000);
+                    return Task.FromResult(value++);
+                });
+
+            public async Task<int> getValueAsync() => await AutoIncValue.Value;
+            public async Task UseValue()
+            {
+                int value = await AutoIncValue.Value;
+            }
+
+        }
+
+        public static void Drive()
+        {
+
+        }
+    }
+}
